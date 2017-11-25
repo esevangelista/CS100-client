4ea654834e084064e7a5621f57cffd09a1a882e3
hasperglasses <jrsunga2@up.edu.ph>
2017-11-25 17:22:46 +0800

    Update NavBar Branch

---
+++ src/features/profile/Profile.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {Image, Icon, Card} from 'semantic-ui-react';
+import something from '../../assets/profpics/somepicture.jpg'
+
+class Profile extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            images: 0
+        }
+    }
+
+    render(){
+        return (
+            <Card>
+            </Card>
+            )
+    }
+}
