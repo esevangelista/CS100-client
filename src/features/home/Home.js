582adb5b4232e033732d5b4b922ec297f76f20af
harryroxas <harry.roxas11@gmail.com>
2017-11-25 23:44:45 +0800

    Modified UserPost

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -7,21 +7,25 @@ import UserPost from './components/feed/UserPost';
 import UserFeed from './components/feed/UserFeed';
 import UserSuggested from './components/suggested/UserSuggested';
 
+import './style.css';
+
 class Home extends Component {
     render(){
         return(
-            <Grid columns={3} padded>
-                <Grid.Column width={4} style={{"margin-left": 40}}>
-                    <UserProfile />
-                </Grid.Column>
-                <Grid.Column width={7}>
-                    <UserPost />
-                    <UserFeed />
-                </Grid.Column>
-                <Grid.Column width={4}>
-                    <UserSuggested />
-                </Grid.Column>
-            </Grid>
+            <div className="page">
+                <Grid columns={3} padded>
+                    <Grid.Column width={4} style={{"margin-left": 40}}>
+                        <UserProfile />
+                    </Grid.Column>
+                    <Grid.Column width={7}>
+                        <UserPost />
+                        <UserFeed />
+                    </Grid.Column>
+                    <Grid.Column width={4}>
+                        <UserSuggested />
+                    </Grid.Column>
+                </Grid>
+            </div>
         );
     }
 }
--- src/features/home/components/feed/UserPost.js
+++ src/features/home/components/feed/UserPost.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
-import { Segment, Form, Image, Input, Icon, Button } from 'semantic-ui-react';
+import { Segment, Form, Image, Icon, Button } from 'semantic-ui-react';
 import image from '../../../../assets/profpics/somepicture.jpg';
 
 import '../style.css';
 
 const UserPost = () => (
     <Segment>
-        <Form>
-            <Form.Field inline>
-                <Image className="userimg" floated="left" size="mini" src={image} />
-                <Input fluid placeholder="Write a Ping..." icon={<Icon link name="image" />} />
-            </Form.Field>
+        <Form.Group>
+            <Image className="userimg" floated="left" size="mini" src={image} />
+            <Form.TextArea rows={2} autoHeight placeholder="Write a Ping..." style={{"float": "left", "width": "455px", "border-radius": 10, "outline": "none"}}/>
+            <Icon name="image" link size="large" style={{"margin-top": 8, "margin-left": 2}}/>
             <Button fluid type="submit" size="tiny" color="teal" compact circular>PING</Button>
-        </Form>
+        </Form.Group>
     </Segment>
 )
 
