26d9ce0603c2be171d08227ccf2745d73341a6ab
harryroxas <harry.roxas11@gmail.com>
2017-11-27 07:11:33 +0800

    Fix signup and feed

--- src/features/home/components/feed/UserFeed.js
+++ src/features/home/components/feed/UserFeed.js
@@ -12,10 +12,11 @@ class UserFeed extends Component {
                         this.props.feeds.map((feed, index) => {
                             return(
                                 <Feed.Event key={index}>
-                                <Feed.Label image={feed.author.imageUrl} />
+                                <Feed.Label image={feed.authorImg} />
                                 <Feed.Content>
                                     <Feed.Summary>
                                         <Feed.User content={feed.authorName} />
+                                        <Feed.Date content={feed.timestamp} />
                                     </Feed.Summary>
                                     <Feed.Extra text content={feed.content} />
                                     <Feed.Meta>
